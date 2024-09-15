db39796597e2c5c1e7187d4c66e45aa90b8e1b94
Nicozmo3 <91019597+Nicozmo3@users.noreply.github.com>
2024-09-15 18:43:58 +0200

    Update ConfigMac.sh
    update app

--- ConfigMac.sh
+++ ConfigMac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-logiciel=("Firefox" "Arc" "hiddenbar" "batteryboi" "wget" "git" "tree" "htop" "discord" "iterm2" "MonitorControl" "Nextcloud" "Ollama" "Plex" "python" "qbittorrent" "telegram" "TextMate" "Tor-browser" "whatsapp" "wireshark" "visual-studio-code" "balenaetcher" "prismlauncher")
+logiciel=("Firefox" "Arc" "hiddenbar" "batteryboi" "wget" "git" "tree" "htop" "discord" "iterm2" "MonitorControl" "Nextcloud" "Ollama" "Plex" "python" "qbittorrent" "telegram" "TextMate" "Tor-browser" "whatsapp" "wireshark" "visual-studio-code" "balenaetcher" "prismlauncher" "cyberghost-vpn" "DiffusionBee" "Heroic" "Latest" "Msty" "Obsidian" "Ryujinx" "Ryusak" "Steam" "VLC" "whisky")
 logiciel_deselectionner=()
 
 Installation() {
